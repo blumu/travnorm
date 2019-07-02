5394958b9c779a0068dbf758809091696eab36c8
William Blum <wiblum@microsoft.com>
2019-07-01 18:26:12 -0700

    move scope caching in from tree nodes to occurrences

--- travnorm.ts
+++ travnorm.ts
@@ -71,7 +71,7 @@ var omega: AltLambda = app(delta, delta)
 /// Neil Jones's example: J = (\u.u(x u))(\v.v y)
 let neil: AltLambda =
   app(abs(['u'], app('u', app('x', 'u'))),
-      abs(['v'], app('u', 'v')))
+      abs(['v'], app('v', 'y')))
 //////////
 
 
@@ -117,21 +117,12 @@ console.log(printLambdaTerm(identity).prettyPrint)
 console.log(printLambdaTerm(omega).prettyPrint)
 console.log(printLambdaTerm(neil).prettyPrint)
 
-////////////// Tree nodes for alternating AST
-/// Node scope
-enum Scope {
-  /// Internal node
-  Internal,
-  /// External node
-  External
-}
-
 /// Tree node labels
 type Label = "@" | "Var" | identifier[]
 
-function isAppLabel(t: Label): t is "@" { return (t as "@") !== undefined }
-function isVarLabel(t: Label): t is "Var" { return (t as "Var") !== undefined }
-function isAbsLabel(t: Label): t is identifier[] { return (t as identifier[]) !== undefined }
+function isAppLabel(t: Label): t is "@" { return (t as "@") == '@' }
+function isVarLabel(t: Label): t is "Var" { return (t as "Var") == 'Var' }
+function isAbsLabel(t: Label): t is identifier[] { return !isAppLabel(t) && !isVarLabel(t) }
 
 /// Defines an enabling node
 type Enabler = {
@@ -139,21 +130,13 @@ type Enabler = {
   // (1 for the parent node, and so on)
   depth : number
   // The enabling label (index of the variable name for a lambda enabler, child index for a variable/@ enabler)
-  label : number,
-  // Cache the scope of the enabling node (external or internal).
-  // Recoverable from the hereditary enabling relation itself.
-  scope: Scope
-}
-
-/// Type of initial node with no enabler
-enum Initial {
-  InitialNode
+  label : number
 }
 
 // A term tree node
 interface TermNode {
   label : Label
-  enabler : Enabler | Initial
+  enabler : Enabler | "Initial"
   children : TermNode[]
 }
 
@@ -162,10 +145,10 @@ function lambdaTermToTreeNodes(
     root:Abs,
     /// the node of the alternating AST to convert
     t:AltNodes,
-    /// the parent note of t, with associated scope
-    parent:([AltNodes,Scope])|undefined,
+    /// the parent node of t
+    parent: AltNodes | undefined,
     /// the list of binder nodes from the root
-    bindersFromRoot:[Abs,Scope][],
+    bindersFromRoot: Abs[],
     /// map that assigns an index to every free variable seen so far
     freeVariableIndex:identifier[]
 ) : TermNode
@@ -177,11 +160,10 @@ function lambdaTermToTreeNodes(
       bindersFromRoot
       .reverse()
       .map((b,i) => {
-          let [bNode, bScope] = b
-          let j = bNode.abs.indexOf(variableName)
-          return j<0 ? undefined : { node: bNode, depth:2*i+1, label: j, scope: bScope }
+          let j = b.abs.indexOf(variableName)
+          return j<0 ? undefined : { node: b, depth:2*i+1, label: j }
       })
-      .filter(b=> b != undefined)[0];
+      .filter(b=> b !== undefined)[0];
     // no binder -> x is a free variable and its enabler is the root
     if(binder === undefined) {
       // lookup the variable index
@@ -189,43 +171,33 @@ function lambdaTermToTreeNodes(
       if(j<0) {
         j = freeVariableIndex.push(variableName)
       }
-      binder = { label: j, depth:2*bindersFromRoot.length+1, scope: Scope.External }
+      binder = { label: j, depth:2*bindersFromRoot.length-1}
     }
     return {
       label : "Var",
       enabler : binder,
-      children : []
+      children: t.arguments.map(o => lambdaTermToTreeNodes(root, o, t, bindersFromRoot, freeVariableIndex))
     }
   } else if (isApp(t)) {
     let [operator, ...operands] = t.app
     return {
       label : "@",
-      enabler : Initial.InitialNode,
-      children : [lambdaTermToTreeNodes(root, operator, [t,Scope.External], bindersFromRoot, freeVariableIndex)]
-                  .concat(operands.map(o => lambdaTermToTreeNodes(root, o, [t,Scope.External], bindersFromRoot, freeVariableIndex)))
+      enabler : "Initial",
+      children: [lambdaTermToTreeNodes(root, operator, t, bindersFromRoot, freeVariableIndex)]
+                  .concat(operands.map(o => lambdaTermToTreeNodes(root, o, t, bindersFromRoot, freeVariableIndex)))
     }
   } else { //if (isAbs(t)) {
-    /// A root node is external, other nodes have the scope of their parent
-    let parentScope = (parent === undefined) ? Scope.External : parent[1]
     return {
       label : t.abs,
-      enabler : (parent === undefined) ? Initial.InitialNode : { label: 1, depth: 1, scope: parentScope },
-      children : [lambdaTermToTreeNodes(root, t.body, [t,parentScope], bindersFromRoot.concat([t,parentScope]), freeVariableIndex)]
+      enabler: (parent === undefined) ? "Initial" : { label: 1, depth: 1 },
+      children: [lambdaTermToTreeNodes(root, t.body, t, bindersFromRoot.concat([t]), freeVariableIndex)]
     }
   }
-
 }
 
-var t = lambdaTermToTreeNodes(omega, omega, undefined, [], [])
-
-console.log("test")
-
-//printLambdaTerm(omega_alt)
-console.log("ended")
-
+// var t = lambdaTermToTreeNodes(omega, omega, undefined, [], [])
 
 ////////// Justified sequences
-
 
 /// A justification pointer
 type Pointer =
@@ -238,40 +210,45 @@ type Pointer =
 
 /// Ghosts node
 type GhostVar = {
-  label: 'GhostVar',
-  scope: Scope /// cached scope (recoverable)
+  label: 'GhostVar'
 }
 
 type GhostLmd = {
   label: 'GhostLmd',
-  names: identifier[],
-  scope: Scope /// cached scope (recoverable)
+  names: identifier[]
 }
 
 type GhostNode = GhostVar | GhostLmd
 
+/// Node scope
+enum Scope {
+  /// Internal node
+  Internal,
+  /// External node
+  External
+}
+
 /// An node occurrence in a justified sequence
 type Occurrence =
 {
-  /// the node it is an occurrence of
+  /// The node it is an occurrence of
   node: TermNode | GhostNode
-  /// pointer to the justifying occurrence
-  justifier : Pointer | Initial
+  /// Pointer to the justifying occurrence
+  justifier: Pointer | "Initial"
+  /// Cache the scope of the underlying node (external or internal) in the occurrence itself
+  /// (Recoverable from the hereditary justification).
+  scope : Scope
 }
 
-function isGhost(t: TermNode | GhostNode): t is GhostNode { return (t as GhostNode).label !== undefined }
-function isStructural(t: TermNode | GhostNode): t is TermNode { return (t as TermNode).label !== undefined }
+function isGhost(t: TermNode | GhostNode): t is GhostNode { let g = (t as GhostNode); return g.label == 'GhostVar' || g.label == 'GhostLmd' }
+function isStructural(t: TermNode | GhostNode): t is TermNode { return !isGhost(t) }
 
 let isVarOccurrence = (o: Occurrence) => isGhost(o.node) ? o.node.label === 'GhostVar' : isVarLabel(o.node.label)
 let isAbsOccurrence = (o: Occurrence) => isGhost(o.node) ? o.node.label === 'GhostLmd' : isAbsLabel(o.node.label)
 let isAppOccurrence = (o: Occurrence) => isGhost(o.node) ? false : isAppLabel(o.node.label)
 
-let scopeOf = (o: Occurrence) => isGhost(o.node)
-  ? o.node.scope
-  : o.node.enabler == Initial.InitialNode ? <Scope>Scope.External : o.node.enabler.scope
-
-let isInternal = (o: Occurrence) => scopeOf(o) == Scope.Internal
-let isExternal = (o: Occurrence) => scopeOf(o) == Scope.External
+let isInternal = (o: Occurrence) => o.scope == Scope.Internal
+let isExternal = (o: Occurrence) => o.scope == Scope.External
 
 /// A justified sequence of node occurrences
 type JustSeq = Occurrence[]
@@ -288,7 +265,7 @@ function* pview (t:JustSeq) {
     } else if (isAppOccurrence(lastOccurrence)) {
       i--
     } else if (isAbsOccurrence(lastOccurrence)) {
-      if(lastOccurrence.justifier === Initial.InitialNode) {
+      if (lastOccurrence.justifier == "Initial") {
         i=-1 // initial node => end of P-view
       } else {
         i-=lastOccurrence.justifier.distance
@@ -308,7 +285,7 @@ function* pview_delta (t:JustSeq) {
     } else if (isAppOccurrence(lastOccurrence)) {
       k = 1
     } else if (isAbsOccurrence(lastOccurrence)) {
-      if(lastOccurrence.justifier === Initial.InitialNode) {
+      if (lastOccurrence.justifier == "Initial") {
         return // initial node => end of P-view
       } else {
         k = lastOccurrence.justifier.distance
@@ -323,13 +300,13 @@ function* pview_delta (t:JustSeq) {
 /// by enumerating the P-view
 function distanceToJustifier (t:JustSeq, enablerDepth:number)
 {
-  var distance = 0
+  var distance = 1
   for(let d of pview_delta(t)) {
+    enablerDepth--
     if(enablerDepth<=0) {
       break;
     }
     distance += d
-    enablerDepth--
   }
   return distance
 }
@@ -372,41 +349,58 @@ function dynamicArity (t:JustSeq) : number {
 
 type Extension = void | Occurrence | Occurrence[]
 
-function isMaximal(e: Extension): e is void { return (e as void) !== undefined }
-function isDeterministic(t: Extension): t is Occurrence { return (t as Occurrence).node !== undefined }
-function isNondeterministic(t: Extension): t is Occurrence[] { return (t as Occurrence[]).length !== undefined }
+function isDeterministic(e: Extension): e is Occurrence { return (e as Occurrence) !== undefined && (e as Occurrence).node !== undefined}
+function isNondeterministic(e: Extension): e is Occurrence[] { return (e as Occurrence[]) !== undefined && (e as Occurrence[]).length !== undefined }
+function isMaximal(e: Extension): e is void { return !isDeterministic(e) && !isNondeterministic(e) }
+
+function childOf(n: TermNode, childIndex: number): TermNode {
+  if(isVarLabel(n.label)) {
+    return n.children[childIndex]
+  } else if (isAppLabel(n.label)) {
+    return n.children[childIndex+1]
+  } else {
+    throw "Child lookup on lambda node not needed."
+  }
+}
 
 /// extend a traversal
 function extendTraversal (treeRoot:TermNode, t:JustSeq) : (void | Occurrence | Occurrence[])
 {
   let lastIndex = t.length-1
   let lastOccurrence = t[lastIndex]
-  /// Empty sequence
-  if(lastOccurrence === undefined) {
+
+  if (lastOccurrence === undefined) { /// Empty sequence?
     return {
       node: treeRoot,
-      justifier: Initial.InitialNode
+      justifier: "Initial",
+      scope: Scope.External
     }
-  } else if (isStructural(lastOccurrence.node) && isAppOccurrence(lastOccurrence)) {
+  } else if (isStructural(lastOccurrence.node) && isAppLabel(lastOccurrence.node.label) ){
+    //if(!isStructural(lastOccurrence.node))
+//      throw "Impossible: An application node is necessarily structural!"
     return {
       node: lastOccurrence.node.children[0], // traverse the operator node
-      justifier : { distance:1, label: 0 }
+      justifier : { distance:1, label: 0 },
+      scope: Scope.Internal
     }
-  } else if (isVarOccurrence(lastOccurrence)) {
-    if(lastOccurrence.justifier === Initial.InitialNode) {
+  } else if (isGhost(lastOccurrence.node) && lastOccurrence.node.label === 'GhostVar'
+          || isStructural(lastOccurrence.node) && isVarLabel(lastOccurrence.node.label)) {
+    if (lastOccurrence.justifier == "Initial") {
       throw "Impossible: a variable occurrence necessarily has a justifier."
     }
     if (isInternal(lastOccurrence)) {
       let distance = 1+lastOccurrence.justifier.distance
       // Occurrence `m` from the paper, is the node preceding the variable occurrence's justifier.
       let m = t[lastIndex - distance]
+      let a = arity(m)
       return {
-        node: isStructural(m.node)
-              ? m.node.children[lastOccurrence.justifier.label]
-              : { label: "GhostLmd", names: [], scope: scopeOf(m) },
-        justifier: { distance: distance, label: lastOccurrence.justifier.label }
+        node: isStructural(m.node) && lastOccurrence.justifier.label < a
+                ? childOf(m.node, lastOccurrence.justifier.label)
+                : { label: "GhostLmd", names: [] },
+        justifier: { distance: distance, label: lastOccurrence.justifier.label },
+        scope: Scope.External
       }
-    } else {
+    } else { /// external variable
       let da = dynamicArity(t)
       if(da === 0) {
         return
@@ -415,23 +409,37 @@ function extendTraversal (treeRoot:TermNode, t:JustSeq) : (void | Occurrence | O
           Array.apply(null, Array(da))
                .map(function (_,i) { return {
                   node: lastOccurrence.node, //previousNode
-                  justifier : { distance:1, label: i }
+                  justifier : { distance:1, label: i },
+                  scope: Scope.External
                 }})
         return possibleChildren
       }
     }
-  } else {// if (isAbsLabel(lastOccurrence.node.label)) {
+  } else if ((isGhost(lastOccurrence.node) && lastOccurrence.node.label === 'GhostLmd'
+          || isAbsLabel(lastOccurrence.node.label))) {
+
     if(isStructural(lastOccurrence.node)) {
       let bodyNode = lastOccurrence.node.children[0]
-      return {
-        node: bodyNode, // traverse the body of the lambda
-        justifier : bodyNode.enabler == Initial.InitialNode
-                        ? Initial.InitialNode
-                        : { distance: distanceToJustifier(t, bodyNode.enabler.depth),
-                            label: bodyNode.enabler.label }
+      if (bodyNode.enabler == "Initial") {
+        return {
+          node: bodyNode, // traverse the body of the lambda
+          justifier: "Initial",
+          scope: Scope.Internal
+        }
+      } else {
+        let d = distanceToJustifier(t, bodyNode.enabler.depth)
+        let m = t[lastIndex - d]
+        return {
+          node: bodyNode, // traverse the body of the lambda
+          justifier: {
+            distance: d,
+            label: bodyNode.enabler.label
+          },
+          scope: m.scope
+        }
       }
     } else { /// Traverse the child of the ghost lambda node
-      if(lastOccurrence.justifier === Initial.InitialNode) {
+      if (lastOccurrence.justifier == "Initial") {
         throw('Impossible: ghost lambda are always justified.')
       }
       let mu = t[lastIndex-lastOccurrence.justifier.distance]
@@ -440,11 +448,42 @@ function extendTraversal (treeRoot:TermNode, t:JustSeq) : (void | Occurrence | O
       let i = lastOccurrence.justifier.label
       let j = arity(mu) + i - arity(m)
       return {
-        node: { label : "GhostVar", scope: scopeOf(m) },
-        justifier : { distance: justifierDistance, label: j }
+        node: { label : "GhostVar"},
+        justifier : { distance: justifierDistance, label: j },
+        scope : m.scope
       }
     }
+  } else {
+    throw "Impossible!"
   }
+}
+
+function printOccurrence(o:Occurrence) : string {
+  if(isGhost(o.node)){
+    if(o.node.label == "GhostLmd") {
+      return '[$]'
+    } else if (o.node.label == "GhostVar") {
+      return '#'
+    } else {
+      throw "Impossible occurrence"
+    }
+  } else if(isAbsLabel(o.node.label)) {
+    return '[' + o.node.label.join(' ') + ']'
+  } else if (isVarLabel(o.node.label)) {
+    if (o.justifier == "Initial") {
+      throw "Impossible: variable cannot be initial."
+    } else {
+      return '('+o.justifier.distance +','+o.justifier.label+')'
+    }
+  } else if (isAppLabel(o.node.label)) {
+    return '@'
+  } else {
+    throw "Impossible occurrence"
+  }
+}
+
+function printSequence(t:JustSeq) {
+  return t.map(printOccurrence).join('--')
 }
 
 function traverse(treeRoot: TermNode, t: JustSeq) {
@@ -452,11 +491,14 @@ function traverse(treeRoot: TermNode, t: JustSeq) {
   while (true) {
     var next = extendTraversal(treeRoot, t)
     if (isMaximal(next)) {
-      return t;
+      console.log("Maximal traversal reached:" + printSequence(t))
+      return t
     } else if(isDeterministic(next)) {
       t = t.concat(next) // append the traversed occurrence
+      console.log("extended:" + printSequence(t))
     } else {
       /// external variable with multiple non-deterministic choices
+      console.log("External variable reached:" + printSequence(t))
       for(let o of next) {
         let t2 :JustSeq = t.concat(o)
         traverse(treeRoot, t2)
@@ -466,9 +508,12 @@ function traverse(treeRoot: TermNode, t: JustSeq) {
 }
 
 /// Evaluate the term with term tree root treeRoot
-function evaluate(treeRoot: TermNode, t: JustSeq) {
+function evaluate(term: AltLambda) {
+  var treeRoot = lambdaTermToTreeNodes(term, term, undefined, [], [])
   traverse(treeRoot, [])
 }
+
+evaluate(neil)
 
 /// core projection
 
